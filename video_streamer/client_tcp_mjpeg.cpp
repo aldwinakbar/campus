d3793e0c7267147bf2254f34a8bc41b55b52f9bd
aldwinakbar <aldwinakbar@gmail.com>
2015-11-28 17:51:16 +0700

    new code

--- video_streamer/client_tcp_mjpeg.cpp
+++ video_streamer/client_tcp_mjpeg.cpp
@@ -1,50 +1,61 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <sys/time.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h> 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <stdio.h>
 #include <fstream>
-#include <arpa/inet.h>
 #include <vector>
-#include <unistd.h>
-
 
 using namespace std;
 using namespace cv;
-
-char encoded_image[100000];
-vector<uchar> buff;
-vector<int> param = vector<int>(2);
-int clientSocket, portNum, nBytes, bytes, x, server_port;
-struct sockaddr_in serverAddr;
-struct sockaddr_storage serverStorage;
-socklen_t addr_size;
-char* server_ip;
-
-int main(int argc, char** argv){
-
-	// ./client_udp 127.0.0.1 7891 90 0  IP Port Quality Webcam
-	param[0] = CV_IMWRITE_JPEG_QUALITY;			// set tipe encoding 
-	param[1] = atoi(argv[3]);					// set kualitas encoding
-
-	strcpy(server_ip, "127.0.0.1");						// IP Server yang dituju
-	server_port = 7891;	
-        
-    // Initialize Streaming Connection
-	struct  	sockaddr_in serverAddr;
-	socklen_t 	serverAddrLen = sizeof(serverAddr);
-    clientSocket = socket(PF_INET, SOCK_STREAM, 0);
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = inet_addr(server_ip);
-    serverAddr.sin_port = htons(server_port);
-	// Initialize Streaming Connection
-		
-	// Connect to Server
-    connect(clientSocket, (sockaddr*)&serverAddr, serverAddrLen);
+ 
+int main(int argc , char *argv[])
+{
+	// Declare Variable 
+    int sock,nBytes,x;
+    struct sockaddr_in server;
+    char message[100000] , server_reply[2000];
+    vector<uchar> buff;
+    vector<int> param = vector<int>(2); 
     
-	VideoCapture cap(atoi(argv[4]));			// Membuka Kamera
+    
+    // declare image sent configuration value
+    param[0] = CV_IMWRITE_JPEG_QUALITY;			// set tipe encoding 
+	param[1] =90;					// set kualitas encoding
+    
+    //Create socket
+    sock = socket(AF_INET , SOCK_STREAM , 0);
+    if (sock == -1)
+    {
+        printf("Could not create socket");
+    }
+    puts("Socket created");
+     
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_family = AF_INET;
+    server.sin_port = htons(50005);
+ 
+    //Connect to remote server
+    if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        perror("connect failed. Error");
+        return 1;
+    }
+     
+    puts("Connected\n");
+    
+    // ######################### Start video transmit ########################### 
+    VideoCapture cap(0);			// Membuka Kamera
 	cap.set(CV_CAP_PROP_FRAME_WIDTH, 640);		// Set Lebar gambar
 	cap.set(CV_CAP_PROP_FRAME_HEIGHT, 360);		// Set tinggi Gambar
 
@@ -58,20 +69,28 @@ int main(int argc, char** argv){
 	Mat image_capture;
 
 	while(1){
-    cap >> image_capture;				// mengambil gambar dari kamera
-    
-    cvtColor(image_capture, image_capture, CV_8U);
+		cap >> image_capture;				// mengambil gambar dari kamera
 		
-    imencode(".jpeg",image_capture, buff,param);	// meng-encode gambar
-          
-    for (x = 0; x < buff.size(); x++){				// merubah data gambar dari tipe vector ke array
-     encoded_image[x] = buff[x];
+		cvtColor(image_capture, image_capture, CV_8U);
+			
+		imencode(".jpeg",image_capture, buff,param);	// meng-encode gambar
+			  
+		for (x = 0; x < buff.size(); x++){				// merubah data gambar dari tipe vector ke array
+			message[x] = buff[x];
+		}
+		
+		nBytes = buff.size();
+		
+		if( send(sock , message , nBytes , 0) < 0)
+        {
+            puts("Send failed");
+            return 1;
+        }
+	
+		cout<<buff.size()<<" Bytes Sent"<<endl;
+		memset(&message[0], 0, nBytes);			// men-clear buffer gambar
 	}
-    
-	nBytes = buff.size();
-	bytes = send(clientSocket, encoded_image, nBytes, 0);
-	cout<<buff.size()<<" Bytes Sent"<<endl;
-    memset(&encoded_image[0], 0, nBytes);			// men-clear buffer gambar
-	}
-  return 0;
+     
+    close(sock);
+    return 0;
 }
