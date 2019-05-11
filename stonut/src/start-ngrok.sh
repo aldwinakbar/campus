cad4dcd5b4aee2d05e98548a0a81cda364896646
Aldwin Hermanudin <aldwin@hermanudin.com>
2019-05-10 22:06:29 -0700

    adjust to US region and python3

--- stonut/src/start-ngrok.sh
+++ stonut/src/start-ngrok.sh
@@ -4,6 +4,6 @@
 pidof -x ngrok > /dev/null
 if [ $? -eq 1 ]; then
 	echo "Start tunnelling..."
-	/opt/stonut/ngrok tcp --log stdout -region ap 22 > /dev/null &
+	/opt/stonut/ngrok tcp --log stdout 22 > /dev/null &
 	echo "Running tunnel in background..."    
 fi
